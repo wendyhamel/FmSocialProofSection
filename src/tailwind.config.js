5d30e561d0f74e150d816fd0ede308befbfcef51
wendyhamel <whamel@it4web.nl>
2023-05-26 15:28:07 +0200

    Structure

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['League Spartan', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				'magenta-dark': 'hsl(300, 43%, 22%)',
+				'pink': 'hsl(333, 80%, 67%)',
+				'magenta-gray': 'hsl(303, 10%, 53%)',
+				'magenta-grey-light': 'hsl(300, 24%, 96%)',
+				'white': 'hsl(0, 0%, 100%)',
+			}
+		}
+	}
+}
